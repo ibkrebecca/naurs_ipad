5888ed628c2eb1c7cc3598b4353b0b7daec8378d
Ibukunoluwa Naphtali <ibukunoluwanap@gmail.com>
2025-10-13 17:23:10 +0300

    upd

--- app/_components/main/admin/new_class.js
+++ app/_components/main/admin/new_class.js
@@ -4,10 +4,16 @@ import Loader from "@/app/_components/loader";
 import { TickSquare, Trash } from "iconsax-react";
 import { Modal } from "react-bootstrap";
 import { useState } from "react";
+import Image from "next/image";
 
 const NewClass = ({ newClass, onHide }) => {
   const [show, setShow] = useState(!!newClass);
   const [isLoading, setIsLoading] = useState(false);
+  const [rawImage, setRawImage] = useState(null);
+  const [viewImage, setViewImage] = useState(null);
+  const [rawVideo, setRawVideo] = useState(null);
+  const [viewVideo, setViewVideo] = useState(null);
+  const [name, setName] = useState("");
 
   const onNewClass = () => {};
 
@@ -24,7 +30,135 @@ const NewClass = ({ newClass, onHide }) => {
 
       <Modal.Body>
         <div className="container-fluid">
-          <form className="row" onSubmit={onNewClass} id="addClass"></form>
+          <form className="row" onSubmit={onNewClass} id="addClass">
+            <div className="col-md-12">
+              <div className="mb-3">
+                <label className="form-label" htmlFor="name">
+                  Class Name
+                </label>
+
+                <input
+                  type="text"
+                  required
+                  className="form-control cus-form-control"
+                  id="name"
+                  placeholder="dance class"
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+
+              <div className="col-md-12">
+                <div className="mb-3">
+                  <label className="form-label" htmlFor="rawImage">
+                    Class Background Image
+                  </label>
+
+                  <div className={viewImage && "d-flex"}>
+                    {viewImage && (
+                      <div className="position-relative">
+                        <Image
+                          src={viewImage}
+                          width={100}
+                          height={100}
+                          alt="Preview"
+                          className="preview-image me-2"
+                        />
+
+                        <div
+                          onClick={() => setViewImage(null)}
+                          className="pe-active alert alert-danger top-0 start-0 position-absolute py-0 pb-1 px-2 m-1"
+                        >
+                          <Trash
+                            size={16}
+                            color="red"
+                            className="text-danger"
+                          />
+                        </div>
+                      </div>
+                    )}
+
+                    <div className="cus-form-file w-100">
+                      <input
+                        required
+                        type="file"
+                        accept="image/*"
+                        name="viewImage"
+                        className="form-control"
+                        id="viewImage"
+                        onChange={(e) => {
+                          const file = e.target.files[0];
+                          setRawImage(file);
+
+                          if (file) {
+                            const reader = new FileReader();
+                            reader.onload = (e) =>
+                              setViewImage(e.target.result);
+                            reader.readAsDataURL(file);
+                          }
+                        }}
+                      />
+                      <label className="file-label" htmlFor="viewImage" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-12">
+                <div className="mb-3">
+                  <label className="form-label" htmlFor="video">
+                    Class Background Video (optional)
+                  </label>
+
+                  <div className={viewVideo && "d-flex"}>
+                    {viewVideo && (
+                      <div className="position-relative">
+                        <Image
+                          src={viewVideo}
+                          width={100}
+                          height={100}
+                          alt="Preview"
+                          className="preview-image me-2"
+                        />
+
+                        <div
+                          onClick={() => setViewVideo(null)}
+                          className="pe-active alert alert-danger top-0 start-0 position-absolute py-0 pb-1 px-2 m-1"
+                        >
+                          <Trash
+                            size={16}
+                            color="red"
+                            className="text-danger"
+                          />
+                        </div>
+                      </div>
+                    )}
+
+                    <div className="cus-form-file w-100">
+                      <input
+                        type="file"
+                        accept="image/*"
+                        name="viewVideo"
+                        className="form-control"
+                        id="viewVideo"
+                        onChange={(e) => {
+                          const file = e.target.files[0];
+                          setRawVideo(file);
+
+                          if (file) {
+                            const reader = new FileReader();
+                            reader.onload = (e) =>
+                              setViewVideo(e.target.result);
+                            reader.readAsDataURL(file);
+                          }
+                        }}
+                      />
+                      <label className="file-label" htmlFor="viewVideo" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </form>
         </div>
       </Modal.Body>
 
