e52da602628d86f060fdd9e5c5229240134135ef
Ibukunoluwa Naphtali <ibukunoluwanap@gmail.com>
2025-10-13 10:51:42 +0300

    upd

--- app/_components/main/admin/admin.js
+++ app/_components/main/admin/admin.js
@@ -1,15 +1,18 @@
 "use client";
 
 import Siderbar from "@/app/_components/sidebar";
-import { collection, onSnapshot } from "firebase/firestore";
-import { BookSquare, Category, Category2 } from "iconsax-react";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { BookSquare, Category, Category2, Trash } from "iconsax-react";
 import { useEffect, useState } from "react";
 import { db } from "@/app/_components/backend/config";
+import Loader from "@/app/_components/loader";
 
 const Admin = () => {
   const [totalClasses, setTotalClasses] = useState(0);
   const [totalCategories, setTotalCategories] = useState(0);
   const [totalSubCategories, setTotalSubCategories] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+  const [classes, setClasses] = useState(null);
 
   useEffect(() => {
     const unsubscribe = onSnapshot(collection(db, "classes"), (snap) => {
@@ -27,6 +30,20 @@ const Admin = () => {
       const total = snap.size;
       setTotalCategories(total);
     });
+
+    return () => unsubscribe();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(collection(db, "classes"), orderBy("createdOn", "desc")),
+      (snap) => {
+        setIsLoading(false);
+        setClasses(snap.docs.map((doc) => doc.data()));
+      }
+    );
 
     return () => unsubscribe();
 
@@ -74,6 +91,20 @@ const Admin = () => {
               </div>
             </div>
           </div>
+
+          {/* body */}
+          {isLoading && classes === null && (
+            <div className="col-md-12 dash-body d-flex justify-content-center align-items-center">
+              <Loader />
+            </div>
+          )}
+
+          {!isLoading && classes !== null && classes.length === 0 && (
+            <div className="col-md-12 dash-body text-muted text-center d-flex flex-column justify-content-center align-items-center">
+              <Trash color="black" size={50} variant="Bulk" />
+              <p className="mt-4 mb-0">No classes yet</p>
+            </div>
+          )}
         </div>
       </div>
     </main>
